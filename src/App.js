b846fab4d765134f8676eec08bb4b4f88c092d3c
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2020-03-30 03:22:18 +0530

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import NavBar from "./components/navBar";
+
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Movie Cards</h1>
+      <NavBar />
+      <main className="container">
+        <h1>Movie Cards</h1>
+      </main>
     </div>
   );
 }
---
+++ src/components/navBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NavBar() {
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand">React Movie Cards</a>
+        </nav>
+    );
+  }
+  
+  export default NavBar;
