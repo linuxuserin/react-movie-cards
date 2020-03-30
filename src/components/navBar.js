56b88aa99a843ad94d2f1c123f95877836426029
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2020-03-31 00:05:16 +0530

    update navbar

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
-function NavBar() {
+function NavBar(props) {
     return (
-        <nav className="navbar navbar-light bg-light">
-            <a className="navbar-brand">React Movie Cards</a>
-        </nav>
+        <nav className="header navbar navbar-dark bg-dark">
+        <div className="container">
+            <div className="row m-auto">
+                <i className="fa fa-film fa-2x text-white my-auto"></i>
+                <div className="h3 ml-3 my-auto text-light" href="/">React Movie Cards</div>
+            </div>
+        </div>
+    </nav>
     );
   }
   
